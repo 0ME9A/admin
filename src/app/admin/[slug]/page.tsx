34c4208be0c8ee42f29c9a0d1ba9e35a674242ec
Baliram Singh <heyome9a@gmail.com>
2024-10-27 12:59:30 +0530

    fixed

--- src/app/admin/[slug]/page.tsx
+++ src/app/admin/[slug]/page.tsx
@@ -1,3 +1,6 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>My Post: {params.slug}</div>;
+export default function Page() {
+  return <div>Status: Coming soon.</div>;
 }
+// export default function Page({ params }: { params: { slug: string } }) {
+//   return <div>My Post: {params.slug}</div>;
+// }
